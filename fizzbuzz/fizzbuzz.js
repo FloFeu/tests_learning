ae7b2c9497549f0c8743b09e1631ca603393ae92
Florent Feugere <florent.feugere@gmail.com>
2021-11-09 20:59:39 +0100

    Improved FizzBuzz Logic

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -2,29 +2,22 @@
  * @param {number} input 
  * @returns {string}
  */
-
 const fizzbuzz = (input) => {
-    if (isMultipleOf3(input) && isMultipleOf5(input)) {
+    if (isMultipleOf(input, 15)) {
         return "fizzbuzz"
-    } else if (isMultipleOf3(input)) {
+    } else if (isMultipleOf(input, 3)) {
         return "fizz"
-    } else if (isMultipleOf5(input)) {
+    } else if (isMultipleOf(input, 5)) {
         return "buzz"
     } else {
         return input.toString()
     }
 }
 
-const isMultipleOf3 = (input) => {
-    if (input % 3 === 0) {
+const isMultipleOf = (input, modulo) => {
+    if (input % modulo === 0) {
         return true
     }
 }
 
-const isMultipleOf5 = (input) => {
-    if (input % 5 === 0) {
-        return true
-    }
-}
-
-module.exports = fizzbuzz;
+module.exports = { fizzbuzz, isMultipleOf }
--- fizzbuzz/fizzbuzz.test.js
+++ fizzbuzz/fizzbuzz.test.js
@@ -1,19 +1,28 @@
-const fizzbuzz = require('./fizzbuzz.js')
+const fizzbuzzLib = require('./fizzbuzz.js')
 
 describe('FizzBuzz Unit Test Suites', () => {
     it("should return fizz when input is a multiple of 3", () => {
-        expect(fizzbuzz(3)).toBe("fizz")
+        expect(fizzbuzzLib.fizzbuzz(3)).toBe("fizz")
     })
 
     it("should return buzz when input is a multiple of 5", () => {
-        expect(fizzbuzz(10)).toBe("buzz")
+        expect(fizzbuzzLib.fizzbuzz(10)).toBe("buzz")
     })
 
     it("should return fizzbuzz when input is a multiple of 3 and 5", () => {
-        expect(fizzbuzz(30)).toBe("fizzbuzz")
+        expect(fizzbuzzLib.fizzbuzz(30)).toBe("fizzbuzz")
     })
 
     it("should return input if it doesn't check any cases", () => {
-        expect(fizzbuzz(1)).toBe('1')
+        expect(fizzbuzzLib.fizzbuzz(1)).toBe('1')
+    })
+
+    it('should return true if the number param is divisible by the modulo param', () => {
+        expect(fizzbuzzLib.isMultipleOf(3, 3)).toBeTruthy()
+    })
+
+    it('should return false if the number param is not divisible by the modulo param', () => {
+        expect(fizzbuzzLib.isMultipleOf(4, 3)).toBeFalsy()
     })
 })
+
